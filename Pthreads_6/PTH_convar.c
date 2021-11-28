0bfa7113ac8b51d88b70683ee2c6048d73cb17ed
Yuxin916 <caiyuxin001220@gmail.com>
2021-11-28 16:37:30 +0800

    add comment

--- Pthreads_6/PTH_convar.c
+++ Pthreads_6/PTH_convar.c
@@ -1,16 +1,14 @@
 //**************************************************************
 //	Demonstrates the usage of pthread conditional variables
 //****************************************************************
-// pthread_cond_wait 
-
-// pthread_cond_signal
-
+// pthread_cond_wait --> wait on conditional variable
+// pthread_cond_signal --> unblock a thread that is waiting on con-var 
 
 #include <stdio.h>
 #include <pthread.h>
-//macro 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+ 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //macro
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER; //macro
 
 int condition = 0;
 int count = 0;
@@ -48,6 +46,8 @@ void*  produce( void * arg )
 
 int main( void )
 {
-   pthread_create( NULL, NULL, &produce, NULL );
-   return consume();
+   pthread_create( NULL, NULL, &produce, NULL ); // create thread 2
+   consume(); // thread 1
+
+   return (0);
 }
